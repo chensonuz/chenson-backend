1d884ca8253144456d4a7449cc5445e5ad37442e
Anvar Khamidov <akhamidov@axevil.com>
2024-01-05 15:59:25 +0500

    hotfix: migrations script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Applying Migrations"
-alembic upgrade head
+poetry run alembic upgrade head
 
 echo "Starting FastAPI..."
-uvicorn --host 0.0.0.0 --port $PORT --log-level 'debug' "$@" main:create_app
+poetry run uvicorn --host 0.0.0.0 --port $PORT --log-level 'debug' "$@" main:create_app
 #uvicorn --env-file './.env' --host 0.0.0.0 --port 8000 --workers 9 --log-level 'debug' "$@" run:create_app
